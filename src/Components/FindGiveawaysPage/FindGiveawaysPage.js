d4b0da62e9599109ec4611d08826b660fa3c4af0
Daniil <flyjeray.dev@gmail.com>
2022-05-15 15:02:51 +0300

    Interaction with API is set

--- src/Components/FindGiveawaysPage/FindGiveawaysPage.js
+++ src/Components/FindGiveawaysPage/FindGiveawaysPage.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import "./FindGiveawaysPage.css"
+import api_data from "../../api-data"
 
 function FindGiveawaysPage() {
+  const options = {
+    method: 'GET',
+    url: 'https://gamerpower.p.rapidapi.com/api/giveaways',
+    params: {platform: 'pc'},
+    headers: {
+      'X-RapidAPI-Host': 'gamerpower.p.rapidapi.com',
+      'X-RapidAPI-Key': api_data.apikey
+    }
+  };
+
+  const pullGiveaways = async () => {
+    const data = await fetch(options.url, options);
+    console.log(data);
+    const json = await data.json();
+    console.log(json);
+  }
+
+  pullGiveaways();
+
   return(
     <h1 className="giveaways_title">Giveaways</h1>
   )
